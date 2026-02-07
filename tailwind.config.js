68aee34e91e84010c29bac077a00db0c9755716d
likevanilla <wjdgur27507@gmail.com>
2026-02-07 17:45:27 +0900

    :lipstick: Design: primary.50 컬러 토큰 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+
+// Design System Colors
+const colors = {
+  primary: {
+    50: "#FFECE8",
+    100: "#FFCFC5",
+    200: "#FFA492",
+    300: "#FF8E78",
+    main: "#FF795E",
+    500: "#EF6346",
+  },
+  secondary: {
+    blue: "#62B1FF",
+    yellow: "#FFC74D",
+  },
+  grayscale: {
+    G500: "#888888",
+    G200: "#CFCFCF",
+  },
+  text: {
+    DEFAULT: "#111111",
+    inverse: "#FFFFFF",
+    sub: "#505050",
+    sub2: "#767676",
+    disabled: "#999999",
+  },
+};
+
 module.exports = {
   presets: [require("nativewind/preset")],
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
@@ -28,27 +56,10 @@ module.exports = {
         // 24pt
         24: ["24px", { lineHeight: "36px" }],
       },
-      colors: {
-        primary: {
-          100: "#FFCFC5",
-          200: "#FFA492",
-          300: "#FF8E78",
-          main: "#FF795E",
-          500: "#EF6346",
-        },
-        secondary: {
-          blue: "#62B1FF",
-          yellow: "#FFC74D",
-        },
-        text: {
-          DEFAULT: "#111111",
-          inverse: "#FFFFFF",
-          sub: "#505050",
-          sub2: "#767676",
-          disabled: "#999999",
-        },
-      },
+      colors: colors,
     },
   },
   plugins: [],
 };
+
+module.exports.colors = colors;
