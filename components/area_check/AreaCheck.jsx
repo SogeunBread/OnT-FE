54a7eb1ccc48095bb24a76920de72ff1c5b0e8d6
likevanilla <wjdgur27507@gmail.com>
2026-02-09 23:06:01 +0900

    :sparkles: Feat: AreaCheck 컴포넌트 구현 (#1)

--- components/area_check/AreaCheck.jsx
+++ components/area_check/AreaCheck.jsx
@@ -1,8 +1,35 @@
-import React from "react";
-import { View } from "react-native";
+import Checkbox from "@/components/checkbox/Checkbox";
+import { Pressable, Text, View } from "react-native";
 
-const AreaCheck = () => {
-  return <View />;
+const AreaCheck = ({
+  label = "지역 이름",
+  checked = false,
+  onPress,
+  className = "",
+  labelClassName = "",
+  ...props
+}) => {
+  return (
+    <Pressable
+      {...props}
+      onPress={onPress}
+      className={`flex-row items-center gap-2 rounded-lg px-4 py-3 ${
+        checked ? "bg-primary-50" : "bg-gray-100"
+      } ${className}`}
+    >
+      <View pointerEvents="none">
+        <Checkbox checked={checked} />
+      </View>
+
+      <Text
+        className={`text-14 font-pretendard-medium ${
+          checked ? "text-primary-main" : "text-text"
+        } ${labelClassName}`}
+      >
+        {label}
+      </Text>
+    </Pressable>
+  );
 };
 
 export default AreaCheck;
