f0ecd8859e9f9e8351bed9fb0c032c1390db5f6d
likevanilla <wjdgur27507@gmail.com>
2026-02-09 21:19:58 +0900

    :recycle: Refactor: Chip 컴포넌트를 부모 상태 관리 구조로 변경 (#1)

--- components/chip/Chip.jsx
+++ components/chip/Chip.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { colors } from "@/tailwind.config";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import React from "react";
+import { Pressable, Text } from "react-native";
 
 const Chip = ({
   text = "선택한 지역",
@@ -11,26 +13,16 @@ const Chip = ({
   closeClassName = "",
   ...props
 }) => {
-  const [visible, setVisible] = useState(true);
-
-  if (!visible) {
-    return null;
-  }
-
   const handleClose = (e) => {
     e?.stopPropagation?.();
-    setVisible(false);
-    if (onClose) {
-      onClose(e);
-    }
+    onClose?.(e);
   };
 
   return (
     <Pressable
       {...props}
-      disabled={disabled}
       onPress={onPress}
-      className={`self-start flex-row items-center justify-start gap-2 rounded-[20px] bg-red-400 px-2 py-1 ${disabled ? "opacity-60" : ""} ${className}`}
+      className={`self-start flex-row items-center gap-2 rounded-[20px] bg-primary-main px-2 py-1 ${disabled ? "opacity-60" : ""} ${className}`}
     >
       <Text
         className={`text-xs font-pretendard-semibold leading-4 text-white ${textClassName}`}
@@ -41,12 +33,10 @@ const Chip = ({
       <Pressable
         disabled={disabled}
         onPress={handleClose}
+        hitSlop={10}
         className={`h-3 w-3 items-center justify-center rounded-[20px] bg-white p-0.5 ${closeClassName}`}
       >
-        <View className="relative h-2 w-2">
-          <View className="absolute left-0 top-[3.5px] h-[1px] w-2 rotate-45 bg-red-400" />
-          <View className="absolute left-0 top-[3.5px] h-[1px] w-2 -rotate-45 bg-red-400" />
-        </View>
+        <AntDesign name="close" size={8} color={colors.primary.main} />
       </Pressable>
     </Pressable>
   );
