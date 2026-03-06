ed1b0d3b51360f7fc4ec53a5272e258bead7db9d
likevanilla <wjdgur27507@gmail.com>
2026-03-06 19:07:19 +0900

    :sparkles: Feat: Tag 컴포넌트 구현 (#1)

--- components/tag/Tag.jsx
+++ components/tag/Tag.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
-const Tag = () => {
-  return <View />;
+const Tag = ({ text = "#근력증가" }) => {
+  return (
+    <View className="bg-white border border-secondary-blue rounded-full px-3 py-1.5 self-start flex-row items-center justify-center">
+      <Text className="text-12 font-pretendard-semibold text-secondary-blue">
+        {text}
+      </Text>
+    </View>
+  );
 };
 
 export default Tag;
