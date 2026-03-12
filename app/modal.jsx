fd037af01902917cacbd75bea3711d4c4de8ce7e
likevanilla <wjdgur27507@gmail.com>
2026-03-12 15:23:48 +0900

    :sparkles: Feat: 모달 컴포넌트 및 화면 추가 (#1)

---
+++ app/modal.jsx
@@ -0,0 +1,10 @@
+import Modal from "@/components/modal";
+import { View } from "react-native";
+
+export default function ModalScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-[#d9d9d9] px-[8px]">
+      <Modal title="타이틀" content="본문 내용" />
+    </View>
+  );
+}
