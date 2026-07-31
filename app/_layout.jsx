a4f31ab7b9b9f6ac70121d1372d28c697dbcc635
likevanilla <wjdgur27507@gmail.com>
2026-07-31 18:16:01 +0900

    :fire: Refactor: 캘린더 관련 컴포넌트 삭제 및 레이아웃 수정 (#9)

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -9,7 +9,6 @@ export default function RootLayout() {
 
       {/* 탭 밖 화면 (오류 발생 시 화면) 404~ */}
       <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-      <Stack.Screen name="calendar" />
     </Stack>
   );
 }
